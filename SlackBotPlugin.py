f29a58abc1ecd083ed3e26b98aa832bb2e397e46
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2020-05-04 15:01:25 +0900

    added format_json function

--- SlackBotPlugin.py
+++ SlackBotPlugin.py
@@ -3,15 +3,36 @@
 #
 # FileName: 	SlackBotPlugin
 # CreatedDate:  2019-07-19 12:32:53 +0900
-# LastModified: 2019-07-27 09:33:25 +0900
+# LastModified: 2020-05-04 14:58:27 +0900
 #
 
+import json
 import re
 
 import pigpio
 from slackbot.bot import listen_to, respond_to
 
 from irrp_class import irrp
+
+
+def format_json():
+    file_name = 'pigpio.json'
+    with open(file_name, 'r') as f:
+        d = json.load(f)
+
+    d_new = dict()
+    for k in d.keys():
+        if len(d[k]) > max_length:
+            l = len(d[k]) // max_length + 1
+            List = d[k]
+            for i in range(l):
+                d_new[f'{k}{i}'] = \
+                    List[max_length*i:(max_length*(i+1) if i != l - 1 else len(List))]
+        else:
+            d_new[k] = d[k]
+
+    with open(file_name, 'w') as f:
+        json.dump(d_new, f, indent=4)
 
 
 @listen_to('Record .*')
@@ -33,6 +54,9 @@ def record(message, *something):
 
     message.reply(f'Recorded {name}')
 
+    format_json()
+    message.reply(f'Formatted {name}')
+
     pi.stop()
 
 
@@ -50,7 +74,7 @@ def play(message, *something):
     ir = irrp(pi,
               GPIO=17,
               FILE='pigpio.json',
-              ID=[f'{name}1', f'{name}2', f'{name}3'],
+              ID=[f'{name}0', f'{name}1'],
               POST=130,
               GAP=1)
     ir.playback()
